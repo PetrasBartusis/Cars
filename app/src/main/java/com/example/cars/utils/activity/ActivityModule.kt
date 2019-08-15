e7bd0f283c13088b3dc1a876f769e0d5b8ee25fb
PBartusis <p.bartusis@gmail.com>
2019-08-15 11:15:02 +0300

    Use MainModule In ActivityModule

--- app/src/main/java/com/example/cars/utils/activity/ActivityModule.kt
+++ app/src/main/java/com/example/cars/utils/activity/ActivityModule.kt
@@ -1,11 +1,12 @@
 package com.example.cars.utils.activity
 
 import com.example.cars.MainActivity
+import com.example.cars.MainModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
 @Module
 abstract class ActivityModule {
-    @ContributesAndroidInjector(modules = [])
+    @ContributesAndroidInjector(modules = [MainModule::class])
     abstract fun contributeMainActivity(): MainActivity
 }
