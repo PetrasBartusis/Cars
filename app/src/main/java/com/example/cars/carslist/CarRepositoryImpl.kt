0d639cf63ead9c11457c5bc6105bb94e6c6ea2b5
PBartusis <p.bartusis@gmail.com>
2019-08-16 09:25:07 +0300

    Update CarRepository

--- app/src/main/java/com/example/cars/carslist/CarRepositoryImpl.kt
+++ app/src/main/java/com/example/cars/carslist/CarRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package com.example.cars.carslist
 
 import com.example.cars.entities.CarModel
-import com.example.cars.utils.factory.CarModelFactory
 import com.example.cars.utils.network.CarsService
 import io.reactivex.Observable
 import io.reactivex.Scheduler
@@ -9,17 +8,12 @@ import javax.inject.Inject
 import javax.inject.Named
 
 class CarRepositoryImpl @Inject constructor(
-        private val carsService: CarsService,
-        private val carModelFactory: CarModelFactory
-): CarRepository {
+        private val carsService: CarsService
+) : CarRepository {
     @field:[Inject Named("io")]
     lateinit var ioScheduler: Scheduler
 
     override fun getCarList(): Observable<List<CarModel>> {
-        return carsService.getAvailableCars()
-                .subscribeOn(ioScheduler)
-                .map { carModelList ->
-            carModelList.map { carModelFactory.transformToCarModel(it) }
-        }
+        return carsService.getCarList()
     }
 }
