55da85c128d3ee6f26f86f80aa396ed51e37ac63
PBartusis <p.bartusis@gmail.com>
2019-08-16 11:08:33 +0300

    Add MapViewModel To MapFragment Via MainModule

--- app/src/main/java/com/example/cars/MainModule.kt
+++ app/src/main/java/com/example/cars/MainModule.kt
@@ -2,6 +2,8 @@ package com.example.cars
 
 import com.example.cars.carslist.CarListFragment
 import com.example.cars.carslist.CarListModule
+import com.example.cars.map.MapFragment
+import com.example.cars.map.MapModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -9,4 +11,7 @@ import dagger.android.ContributesAndroidInjector
 abstract class MainModule private constructor() {
     @ContributesAndroidInjector(modules = [CarListModule::class])
     abstract fun contributesCarListFragment(): CarListFragment
+
+    @ContributesAndroidInjector(modules = [MapModule::class])
+    abstract fun contributesMapFragment(): MapFragment
 }
