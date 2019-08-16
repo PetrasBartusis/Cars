ac440c630ce46df53cb9a5894373a3bcd709de89
PBartusis <p.bartusis@gmail.com>
2019-08-16 09:24:38 +0300

    Separate Service And API Class

---
+++ app/src/main/java/com/example/cars/utils/network/CarsApi.kt
@@ -0,0 +1,10 @@
+package com.example.cars.utils.network
+
+import com.example.cars.utils.network.entities.CarServiceModel
+import io.reactivex.Observable
+import retrofit2.http.GET
+
+interface CarsApi {
+    @GET("api/mobile/public/availablecars")
+    fun getAvailableCars(): Observable<List<CarServiceModel>>
+}
--- app/src/main/java/com/example/cars/utils/network/CarsService.kt
+++ app/src/main/java/com/example/cars/utils/network/CarsService.kt
@@ -1,10 +1,24 @@
 package com.example.cars.utils.network
 
-import com.example.cars.utils.network.entities.CarServiceModel
+import com.example.cars.entities.CarModel
+import com.example.cars.utils.factory.CarModelFactory
 import io.reactivex.Observable
-import retrofit2.http.GET
+import io.reactivex.Scheduler
+import javax.inject.Inject
+import javax.inject.Named
 
-interface CarsService {
-    @GET("api/mobile/public/availablecars")
-    fun getAvailableCars(): Observable<List<CarServiceModel>>
+class CarsService @Inject constructor(
+        private val carsService: CarsApi,
+        private val carModelFactory: CarModelFactory
+) {
+    @field:[Inject Named("io")]
+    lateinit var ioScheduler: Scheduler
+
+    fun getCarList(): Observable<List<CarModel>> {
+        return carsService.getAvailableCars()
+                .subscribeOn(ioScheduler)
+                .map { carModelList ->
+                    carModelList.map { carModelFactory.transformToCarModel(it) }
+                }
+    }
 }
