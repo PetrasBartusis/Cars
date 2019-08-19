cb33fa339c71a680c48bdc4f79d0463ae5ca9551
PBartusis <p.bartusis@gmail.com>
2019-08-19 21:56:12 +0300

    Inject Location Manager To MainModule

--- app/src/main/java/com/example/cars/MainModule.kt
+++ app/src/main/java/com/example/cars/MainModule.kt
@@ -4,8 +4,14 @@ import com.example.cars.carslist.CarListFragment
 import com.example.cars.carslist.CarListModule
 import com.example.cars.map.MapFragment
 import com.example.cars.map.MapModule
+import com.example.cars.utils.location.DefaultLocationManager
+import com.example.cars.utils.location.LocationManager
+import com.tbruyelle.rxpermissions2.RxPermissions
+import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.android.ContributesAndroidInjector
+import pl.charmas.android.reactivelocation2.ReactiveLocationProvider
 
 @Module
 abstract class MainModule private constructor() {
@@ -14,4 +20,20 @@ abstract class MainModule private constructor() {
 
     @ContributesAndroidInjector(modules = [MapModule::class])
     abstract fun contributesMapFragment(): MapFragment
+
+    @Binds
+    abstract fun bindLocationManager(locationManager: DefaultLocationManager): LocationManager
+
+    @Module
+    companion object {
+        @JvmStatic
+        @Provides
+        fun provideReactiveLocationProvider(mainActivity: MainActivity) =
+                ReactiveLocationProvider(mainActivity)
+
+        @JvmStatic
+        @Provides
+        fun provideRxPermissions(mainActivity: MainActivity) =
+                RxPermissions(mainActivity)
+    }
 }
