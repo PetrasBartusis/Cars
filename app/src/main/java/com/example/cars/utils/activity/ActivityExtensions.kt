feca0fd24c4ab6f38d546ac80a430c24865ecbc3
PBartusis <p.bartusis@gmail.com>
2019-08-19 20:13:34 +0300

    Add Build Filter Dialog Extension

--- app/src/main/java/com/example/cars/utils/activity/ActivityExtensions.kt
+++ app/src/main/java/com/example/cars/utils/activity/ActivityExtensions.kt
@@ -1,8 +1,11 @@
 package com.example.cars.utils.activity
 
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.FragmentActivity
 import com.example.cars.MainActivity
+import com.example.cars.utils.dialog.FilterLayout
+import com.example.cars.utils.dialog.ViewDialogBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 
 fun FragmentActivity.showMessage(message: String) {
@@ -15,4 +18,30 @@ fun FragmentActivity.showMessage(messageResId: Int) {
 
 fun FragmentActivity.openDrawer() {
     drawer.openDrawer(GravityCompat.START)
+}
+
+fun FragmentActivity.carFilterDialog(apply: ViewDialogBuilder.() -> Unit): AlertDialog {
+    val builder = ViewDialogBuilder()
+    builder.apply()
+    val carFilterLayout = FilterLayout(this, null)
+
+    val dialog = AlertDialog.Builder(this).apply {
+        setView(carFilterLayout)
+        setTitle(builder.title)
+        setMessage(builder.message)
+        setPositiveButton(builder.positiveButton) { _, _ ->
+            builder.success.invoke(carFilterLayout.value)
+        }
+        setNegativeButton(builder.negativeButton) { _, _ ->
+            builder.failure.invoke()
+        }
+        setNeutralButton(builder.neutralButton) { _, _ ->
+            builder.neutral.invoke()
+        }
+        setOnCancelListener {
+            builder.failure.invoke()
+        }
+        builder.view?.let { setView(builder.view) }
+    }
+    return dialog.show()
 }
