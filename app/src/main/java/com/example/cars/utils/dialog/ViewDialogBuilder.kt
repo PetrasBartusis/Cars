4ede03eb104b4f7d93998837ecc35cef5cb6aaed
PBartusis <p.bartusis@gmail.com>
2019-08-19 20:12:59 +0300

    Add ViewDialogBuilder

---
+++ app/src/main/java/com/example/cars/utils/dialog/ViewDialogBuilder.kt
@@ -0,0 +1,15 @@
+package com.example.cars.utils.dialog
+
+import android.view.View
+
+class ViewDialogBuilder internal constructor() {
+    var view: View? = null
+    var title: String? = null
+    var message: String? = null
+    var positiveButton: String? = null
+    var negativeButton: String? = null
+    var neutralButton: String? = null
+    var success: (FilterOptions) -> Unit = {}
+    var failure: () -> Unit = {}
+    var neutral: () -> Unit = {}
+}
