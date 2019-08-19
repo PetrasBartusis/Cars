1a92a64ff7695d26c83de8f4e0c508b9e759e1f3
PBartusis <p.bartusis@gmail.com>
2019-08-19 20:12:16 +0300

    Add View To Filter Cars

---
+++ app/src/main/java/com/example/cars/utils/dialog/FilterOptions.kt
@@ -0,0 +1,7 @@
+package com.example.cars.utils.dialog
+
+data class FilterOptions(
+        var plateNumberFilter: String = "",
+        var remainingBatteryFilter: Int = 0,
+        var distanceFilter: Boolean = false
+)
